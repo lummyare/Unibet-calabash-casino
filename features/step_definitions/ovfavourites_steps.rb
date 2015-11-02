7a5eee23836bf012566cb763ba0734b6f0857f30
Lummyare <lummyare@gmail.com>
2015-11-02 12:53:38 +0000

    Remove "args" from step definitions

--- features/step_definitions/ovfavourites_steps.rb
+++ features/step_definitions/ovfavourites_steps.rb
@@ -22,74 +22,74 @@ Then(/^I launch OFgame6 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame7 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame7 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame8 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame8 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame9 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame9 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame10 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame10 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame11 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame11 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame12 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame12 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame13 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame13 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame14 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame14 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame15 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame15 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame16 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame16 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame17 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame17 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame18 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame18 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame19 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame19 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame20 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame20 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame21 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame21 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame22 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame22 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame23 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame23 in Overall Favourites Games$/) do
   pending
 end
 
-Then(/^I launch OFgame24 in Overall Favourites Games$/) do |arg|
+Then(/^I launch OFgame24 in Overall Favourites Games$/) do
   pending
 end
